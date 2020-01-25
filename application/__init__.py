b0f54a1447231aba0e9b5541d99c9cd000e51d15
Juuso Kauppala <juuso.kauppala@helsinki.fi>
2020-01-25 22:17:08 +0200

    Fixing issues

--- application/__init__.py
+++ application/__init__.py
@@ -3,9 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # Database
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gtgapp.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"    
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -35,4 +39,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 # Create tables
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
