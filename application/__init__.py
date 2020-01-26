adae14a3074c88b770f15b53acbf189771f8de41
Juuso Kauppala <juuso.kauppala@helsinki.fi>
2020-01-26 14:01:41 +0200

    database configuration

--- application/__init__.py
+++ application/__init__.py
@@ -3,18 +3,18 @@ import os
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from flask_sqlalchemy import SQLAlchemy
 
 # Database
-if os.environ.get("HEROKU"):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gtgapp.db"    
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["SQLALCHEMY_ECHO"] = True
+#if os.environ.get("HEROKU"):
+#    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+#    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#else:
+#    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///gtgapp.db"    
+#    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
+import os
 from application import app, db 
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 migrate = Migrate(app, db)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
